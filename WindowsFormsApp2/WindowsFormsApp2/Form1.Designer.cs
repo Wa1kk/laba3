785e3b2a944d290c11d70b5a16ee2c8b7d228024
Wa1kk <35396890+Wa1kk@users.noreply.github.com>
2024-11-27 00:30:25 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.Button btnLoadOverlay;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        private void InitializeComponent()
+        {
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.btnLoadOverlay = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(150, 23);
+            this.btnLoadImage.TabIndex = 0;
+            this.btnLoadImage.Text = "Загрузить изображение";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // btnLoadOverlay
+            // 
+            this.btnLoadOverlay.Location = new System.Drawing.Point(12, 41);
+            this.btnLoadOverlay.Name = "btnLoadOverlay";
+            this.btnLoadOverlay.Size = new System.Drawing.Size(150, 23);
+            this.btnLoadOverlay.TabIndex = 1;
+            this.btnLoadOverlay.Text = "Загрузить изображение 2";
+            this.btnLoadOverlay.UseVisualStyleBackColor = true;
+            this.btnLoadOverlay.Click += new System.EventHandler(this.btnLoadOverlay_Click);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(12, 70);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(150, 23);
+            this.btnProcess.TabIndex = 2;
+            this.btnProcess.Text = "Наложить";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 99);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(150, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(180, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(400, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(620, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(840, 230);
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.btnLoadOverlay);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Name = "Form1";
+            this.Text = "Попиксельные преобразования";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap originalImage;
+        private Bitmap overlayImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    originalImage = new Bitmap(openFileDialog.FileName);
+                    pictureBox1.Image = originalImage;
+                }
+            }
+        }
+
+        private void btnLoadOverlay_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    overlayImage = new Bitmap(openFileDialog.FileName);
+                    pictureBox2.Image = overlayImage;
+                }
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null || overlayImage == null)
+            {
+                MessageBox.Show("Please load both images first.");
+                return;
+            }
+
+            Bitmap processedImage = (Bitmap)originalImage.Clone();
+            AdjustContrast(processedImage, 50, 90);
+            OverlayImages(processedImage, overlayImage);
+            pictureBox3.Image = processedImage;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (pictureBox3.Image != null)
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        pictureBox3.Image.Save(saveFileDialog.FileName);
+                    }
+                }
+            }
+        }
+
+        private void AdjustContrast(Bitmap bmp, int minPercent, int maxPercent)
+        {
+            float min = minPercent / 100.0f;
+            float max = maxPercent / 100.0f;
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color pixel = bmp.GetPixel(x, y);
+
+                    int r = Clamp((int)(min * 255 + (pixel.R / 255.0) * (max - min) * 255), 0, 255);
+                    int g = Clamp((int)(min * 255 + (pixel.G / 255.0) * (max - min) * 255), 0, 255);
+                    int b = Clamp((int)(min * 255 + (pixel.B / 255.0) * (max - min) * 255), 0, 255);
+
+                    bmp.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+        }
+
+        private void OverlayImages(Bitmap baseImage, Bitmap overlay)
+        {
+            for (int y = 0; y < baseImage.Height && y < overlay.Height; y++)
+            {
+                for (int x = 0; x < baseImage.Width && x < overlay.Width; x++)
+                {
+                    Color basePixel = baseImage.GetPixel(x, y);
+                    Color overlayPixel = overlay.GetPixel(x, y);
+
+                    int r = (basePixel.R + overlayPixel.R) / 2;
+                    int g = (basePixel.G + overlayPixel.G) / 2;
+                    int b = (basePixel.B + overlayPixel.B) / 2;
+
+                    baseImage.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+        }
+
+        private int Clamp(int value, int min, int max)
+        {
+            return Math.Max(min, Math.Min(max, value));
+        }
+    }
+}
